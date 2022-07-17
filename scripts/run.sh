1acab7fba3954f388ec4055bbdb7fa5eab6c07f0
yashshastri <108329985+yashshastri@users.noreply.github.com>
2022-07-17 19:12:34 +0530

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+# -p defines protocol type
+# -l defines packet loss percentage
+# -d defines delay in ms
+# -b defines bandiwidth mbps
+# -s defines dealy in before spike in seconds
+python comparsion.py -p QUIC TCP -l 5.0 -d 50 -b 100 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 50 -b 40 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 50 -b 5 --vverbose
 
-python comparsion.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 100 --vverbose
-python comparsion.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 40 --vverbose
-python comparsion.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 5 --vverbose
+python comparsion.py -p QUIC TCP -l 0.0 -d 10 -b 100 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 0.0 -d 10 -b 40 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 0.0 -d 10 -b 5 -s 1 --vverbose
 
-python comparsion.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 100 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 40 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 5 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 10 -b 100 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 10 -b 40 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 10 -b 5 -s 1 --vverbose
 
-python comparsion.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 100 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 40 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 5 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 0.0 -d 50 -b 100 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 0.0 -d 50 -b 40 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 0.0 -d 50 -b 5 -s 1 --vverbose
 
-python comparsion.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 100 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 40 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 5 -s 1 --vverbose
-
-python comparsion.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 100 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 40 -s 1 --vverbose
-python comparsion.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 5 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 50 -b 100 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 50 -b 40 -s 1 --vverbose
+python comparsion.py -p QUIC TCP -l 5.0 -d 50 -b 5 -s 1 --vverbose
