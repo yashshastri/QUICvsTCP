a56cee076e236ccf09bb0422bd9b207604e6dab2
yashshastri <108329985+yashshastri@users.noreply.github.com>
2022-07-17 18:43:01 +0530

    Update env_setup.sh

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -3,5 +3,5 @@
 mkdir -p raw processed plt tmp
 # default value setup for mtu
 sudo ifconfig lo mtu 1500
-
+# quic server setup
 /home/csg/software/proto-quic/src/out/Default/quic_server --quic_response_cache_dir=/var/www/html --certificate_file=/home/csg/software/proto-quic/src/net/tools/quic/certs/out/leaf_cert.pem --key_file=/home/csg/software/proto-quic/src/net/tools/quic/certs/out/leaf_cert.pkcs8
